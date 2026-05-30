e01f0708ac5197279b5806ba095c93caf8172ffb
Жанна <67254752@gmail.com>
2026-05-30 17:45:03 +0300

    Init commit

--- app/src/main/java/com/example/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.example.playlistmaker
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -12,5 +16,34 @@ class MainActivity : AppCompatActivity() {
 
         setContentView(R.layout.activity_main)
 
+        val buttonSearch = findViewById<Button>(R.id.btn_search)
+        val buttonMedia = findViewById<Button>(R.id.btn_media)
+        val buttonSettings = findViewById<Button>(R.id.btn_settings)
+
+        val searchClickListener = object : View.OnClickListener{
+            override fun onClick(v: View?) {
+                val searchIntent = Intent(this@MainActivity, SearchActivity::class.java)
+                startActivity(searchIntent)
+
+                //Toast.makeText(this@MainActivity, "Нажата кнопка Поиск", Toast.LENGTH_SHORT).show()
+            }
+
+        }
+
+
+        buttonMedia.setOnClickListener{
+            val mediaIntent = Intent(this, MediaActivity::class.java)
+            startActivity(mediaIntent)
+
+            //Toast.makeText(this, "Нажата кнопка Медиатека", Toast.LENGTH_SHORT).show()
+
+        }
+        buttonSettings.setOnClickListener{
+            val settingIntent = Intent(this, SettingsActivity::class.java)
+            startActivity(settingIntent)
+            //Toast.makeText(this, "Нажата кнопка Настройки", Toast.LENGTH_SHORT).show()
+        }
+
     }
 }
+
---
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -0,0 +1,20 @@
+package com.example.playlistmaker
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+
+class SettingsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_settings)
+
+        val backButton = findViewById<ImageView>(R.id.back_button)
+        backButton.setOnClickListener {
+            val backIntent = Intent(this, MainActivity::class.java)
+            startActivity(backIntent)
+        }
+    }
+}
