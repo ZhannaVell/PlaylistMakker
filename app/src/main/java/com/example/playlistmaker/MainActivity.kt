ac4d0f9bf6f242e73f0b1bee514887a6f67f1894
zhanna <6725475@gmail.com>
2026-06-12 17:58:22 +0300

    Домашняя работа спринта 9

--- app/src/main/java/com/example/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MainActivity.kt
@@ -29,6 +29,8 @@ class MainActivity : AppCompatActivity() {
             }
 
         }
+        buttonSearch.setOnClickListener(searchClickListener)
+
 
 
         buttonMedia.setOnClickListener{
--- app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -1,8 +1,10 @@
 package com.example.playlistmaker
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.widget.ImageView
+import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
 
 class SettingsActivity : AppCompatActivity() {
@@ -12,9 +14,36 @@ class SettingsActivity : AppCompatActivity() {
         setContentView(R.layout.activity_settings)
 
         val backButton = findViewById<ImageView>(R.id.back_button)
+        val shareItem = findViewById<LinearLayout>(R.id.share_item)
+        val supportItem = findViewById<LinearLayout>(R.id.support_item)
+        val agreementItem = findViewById<LinearLayout>(R.id.agreement_item)
 
         backButton.setOnClickListener {
             finish()
         }
+        shareItem.setOnClickListener {
+            val shareIntent = Intent(Intent.ACTION_SEND).apply {
+                type = "text/plain"
+                putExtra(Intent.EXTRA_TEXT,getString(R.string.share_message))
+            }
+            startActivity(Intent.createChooser(shareIntent,null))
+        }
+        supportItem.setOnClickListener {
+            val emailIntent = Intent(Intent.ACTION_SENDTO).apply {
+                data = Uri.parse("mailto:")
+                putExtra(Intent.EXTRA_EMAIL, arrayOf(getString(R.string.support_email)))
+                putExtra(Intent.EXTRA_SUBJECT, getString(R.string.support_subject))
+                putExtra(Intent.EXTRA_TEXT,getString(R.string.support_body))
+
+            }
+            startActivity(Intent.createChooser(emailIntent,null))
+        }
+        agreementItem.setOnClickListener {
+            val termsUrl = getString(R.string.terms_url)
+            val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse(termsUrl))
+            startActivity(browserIntent)
+        }
+
+
     }
 }
